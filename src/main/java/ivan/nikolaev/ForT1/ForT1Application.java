4b4cd944cb80023ae857f2414d9b237af9fc27f3
Duranna66 <ivan.kushnarev.04@list.ru>
2023-09-26 20:34:49 +0300

    start project

---
+++ src/main/java/ivan/nikolaev/ForT1/ForT1Application.java
@@ -0,0 +1,13 @@
+package ivan.nikolaev.ForT1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForT1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForT1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ivan/nikolaev/ForT1/ForT1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ivan.nikolaev.ForT1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForT1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
